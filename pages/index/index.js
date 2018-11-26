195201c2b78291120a1c594d6082ea5b03a13006
853029827@qq.com <853029827@qq.com>
2018-11-26 18:04:21 +0800

    请求接口初始化

---
+++ pages/index/index.js
@@ -0,0 +1,83 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  onLoad: function () {
+    var that = this//不要漏了这句，很重要
+    // https://blog.eunji.cn/api/archives/year
+    // wx.request({
+    //   url: 'https://blog.eunji.cn/api/archives/year', //仅为示例，并非真实的接口地址
+    //   data: {},
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success(res) {
+    //     console.log(res.data)
+    //   }
+    // })
+    wx.request({
+      url: 'https://blog.eunji.cn/api/archives/year',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        //将获取到的json数据，存在名字叫zhihu的这个数组中
+        console.log(res.data.result[0].posts[0].postId)
+        that.setData({
+          zhihu: res.data.stories,
+          //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
+
+        })
+      }
+    })
+  }
+})
