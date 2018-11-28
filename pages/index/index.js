5fee33afe9c73280c5d579e92119e078cf361ce4
853029827@qq.com <853029827@qq.com>
2018-11-28 23:32:50 +0800

    添加Loading
    添加Loading

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,42 +17,32 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  onLoad: function () {
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
     var that = this//不要漏了这句，很重要
     var url = 'https://blog.eunji.cn/api/archives/year'
     var userAvatarUrl = 'https://blog.eunji.cn'
@@ -75,25 +65,21 @@ Page({
         })
       }
     }),
-    jinrishici.load(result => {
-      // 下面是处理逻辑示例
-      console.log(result)
-      this.setData({
-        "jinrishici": result.data.content,
-        shici: result.data.origin.content,
+      jinrishici.load(result => {
+        // 下面是处理逻辑示例
+        console.log(result)
+        this.setData({
+          "jinrishici": result.data.content,
+          shici: result.data.origin.content,
+        })
       })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
-
-    // https://blog.eunji.cn/api/archives/year
-    // wx.request({
-    //   url: 'https://blog.eunji.cn/api/archives/year', //仅为示例，并非真实的接口地址
-    //   data: {},
-    //   header: {
-    //     'content-type': 'application/json' // 默认值
-    //   },
-    //   success(res) {
-    //     console.log(res.data)
-    //   }
-    // })
   }
 })
