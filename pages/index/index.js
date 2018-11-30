de14c8f98ad49ff885e4a493b708f114ea6fbea9
853029827@qq.com <853029827@qq.com>
2018-12-01 01:37:00 +0800

    采用html2wxml解析-废弃wxParse
    采用html2wxml解析-废弃wxParse

--- pages/index/index.js
+++ pages/index/index.js
@@ -34,42 +34,42 @@ Page({
             console.log(res.data.result[0].posts[0])
             that.setData({
 
-            spinShow: false,
-            //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
-            userName: res.data.result[0].posts[0].user.userDisplayName,
-            userDesc: res.data.result[0].posts[0].user.userDesc,
-            userAvatar: userAvatarUrl + res.data.result[0].posts[0].user.userAvatar,
-            title: res.data.result[0].posts[0].postTitle,
-            content: res.data.result[0].posts[0].postContent,
-            posts: res.data.result[0].posts,
+                spinShow: false,
+                //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
+                userName: res.data.result[0].posts[0].user.userDisplayName,
+                userDesc: res.data.result[0].posts[0].user.userDesc,
+                userAvatar: userAvatarUrl + res.data.result[0].posts[0].user.userAvatar,
+                title: res.data.result[0].posts[0].postTitle,
+                content: res.data.result[0].posts[0].postContent,
+                posts: res.data.result[0].posts,
             })
             //取消Loading效果
             // wx.hideLoading()
         }
         })
         jinrishici.load(result => {
-        // 下面是处理逻辑示例
-        console.log(result)
-        this.setData({
-            "jinrishici": result.data.content,
-            shici: result.data.origin.content,
-        })
-        //关闭下拉刷新
-        wx.stopPullDownRefresh()
+            // 下面是处理逻辑示例
+            console.log(result)
+            this.setData({
+                "jinrishici": result.data.content,
+                shici: result.data.origin.content,
+            })
+            //关闭下拉刷新
+            wx.stopPullDownRefresh()
         })
 
     },
     //事件处理函数
     bindViewTap: function() {
         wx.navigateTo({
-        url: '../logs/logs'
+            url: '../logs/logs'
         })
     },
     onSwitchChange({ detail }) {
         const value = detail.value;
         this.setData({
-        switch: value,
-        spinShow: !value
+            switch: value,
+            spinShow: !value
         });
     },
     onLoad: function () {
@@ -82,35 +82,35 @@ Page({
         //   title: '加载中',
         // })
         wx.request({
-        url: url,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        success: function (res) {
-            //将获取到的json数据，存在名字叫zhihu的这个数组中
-            console.log(res.data.result[0].posts[0])
-            that.setData({
+            url: url,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            success: function (res) {
+                //将获取到的json数据，存在名字叫zhihu的这个数组中
+                console.log(res.data.result[0].posts[0])
+                that.setData({
 
-            spinShow: false,
-            //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
-            userName: res.data.result[0].posts[0].user.userDisplayName,
-            userDesc: res.data.result[0].posts[0].user.userDesc,
-            userAvatar: userAvatarUrl + res.data.result[0].posts[0].user.userAvatar,
-            title: res.data.result[0].posts[0].postTitle,
-            content: res.data.result[0].posts[0].postContent,
-            posts: res.data.result[0].posts,
-            })
-            //取消Loading效果
-            // wx.hideLoading()
-        }
+                spinShow: false,
+                //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
+                userName: res.data.result[0].posts[0].user.userDisplayName,
+                userDesc: res.data.result[0].posts[0].user.userDesc,
+                userAvatar: userAvatarUrl + res.data.result[0].posts[0].user.userAvatar,
+                title: res.data.result[0].posts[0].postTitle,
+                content: res.data.result[0].posts[0].postContent,
+                posts: res.data.result[0].posts,
+                })
+                //取消Loading效果
+                // wx.hideLoading()
+            }
         })
         jinrishici.load(result => {
-        // 下面是处理逻辑示例
-        console.log(result)
-        this.setData({
-            "jinrishici": result.data.content,
-            shici: result.data.origin.content,
-        })
+            // 下面是处理逻辑示例
+            console.log(result)
+            this.setData({
+                "jinrishici": result.data.content,
+                shici: result.data.origin.content,
+            })
         })
     },
     getUserInfo: function(e) {
--- pages/post/post.js
+++ pages/post/post.js
@@ -5,49 +5,53 @@ Page({
      * 页面的初始数据
      */
     data: {
-        scrollTop: 0
+        scrollTop: 0,
+        linenums: false,
+        spinShow: true,
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-    this.setData({
-        postId: options.postId,
-    })
-
-    // wx.showNavigationBarLoading() //在标题栏中显示加载
-    var that = this//不要漏了这句，很重要
-    var postId = options.postId
-    var url = 'https://blog.eunji.cn/api/posts/' + postId
-    var userAvatarUrl = 'https://blog.eunji.cn'
-
-    //微信自带Loading效果
-    // wx.showLoading({
-    //   title: '加载中',
-    // })
-    wx.request({
-        url: url,
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        success: function (res) {
-        //将获取到的json数据，存在名字叫zhihu的这个数组中
-        console.log(res.data.result)
-        that.setData({
-            post: res.data.result,
+        this.setData({
+            postId: options.postId,
         })
-        //取消Loading效果
-        // wx.hideLoading()
-        }
-    })
+
+        // wx.showNavigationBarLoading() //在标题栏中显示加载
+        var that = this//不要漏了这句，很重要
+        var postId = options.postId
+        var url = 'https://blog.eunji.cn/api/posts/' + postId
+        var userAvatarUrl = 'https://blog.eunji.cn'
+
+        //微信自带Loading效果
+        // wx.showLoading({
+        //   title: '加载中',
+        // })
+        wx.request({
+            url: url,
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            success: function (res) {
+            //将获取到的json数据，存在名字叫zhihu的这个数组中
+            console.log(res.data.result)
+            that.setData({
+                post: res.data.result,
+            })
+            //取消Loading效果
+            // wx.hideLoading()
+            }
+        })
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        this.setData({
+            spinShow: false,
+        })
     },
 
     /**
