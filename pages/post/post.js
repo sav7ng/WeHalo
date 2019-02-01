ed364cc4e9d29aefc5e11206e54e29d1f75ec284
853029827@qq.com <853029827@qq.com>
2019-02-01 15:55:27 +0800

    :sparkles: 添加微信授权后评论

--- pages/post/post.js
+++ pages/post/post.js
@@ -17,12 +17,50 @@ Page({
         spinShows: '',
         commentsFlag: false,
         style: app.globalData.highlightStyle,
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
+
+
+        /**
+         * 获取用户信息
+         */
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.UserInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
+
+
+
+
 
         page = this;
         this.setData({
@@ -56,8 +94,19 @@ Page({
         //         that.bindbt();
         //     }, 200);
         // }, 2000);
+
     },
 
+    getUserInfo: function (e) {
+        console.log(e)
+        app.globalData.userInfo = e.detail.userInfo;
+        app.globalData.nickName = e.detail.userInfo.nickName;
+        app.globalData.avatarUrl = e.detail.userInfo.avatarUrl;
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
+    },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
@@ -221,14 +270,12 @@ Page({
         var params = {
             postId: postId,
             commentContent: that.data.inputValue,
-            commentAuthor: 'Aquan',
-            commentAuthorEmail: 'aquanlerou@eunji.cn',
-            commentAuthorUrl: 'https://blog.eunji.cn',
+            commentAuthor: app.globalData.nickName,
+            commentAuthorEmail: '',
+            commentAuthorUrl: app.globalData.avatarUrl,
             commentAgent: '1018',
             commentParent: '0',
         };
-        //@todo 文章内容网络请求API数据
-        request.requestPostApi(url, token, params, this, this.successSend, this.failSend);
 
 
         if (that.data.inputValue != null && that.data.inputValue != '') {
@@ -236,6 +283,9 @@ Page({
             that.setData({
                 doommData: doommList
             });
+
+            //@todo 文章内容网络请求API数据
+            request.requestPostApi(url, token, params, this, this.successSend, this.failSend);
         } else {
             $Message({
                 content: '请输入吐槽内容',
