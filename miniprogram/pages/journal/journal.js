32c56f42bf33192b353249cb5509c78ade809313
853029827@qq.com <853029827@qq.com>
2020-04-19 17:29:48 +0800

    :art: 添加日记本页面

--- miniprogram/pages/journal/journal.js
+++ miniprogram/pages/journal/journal.js
@@ -64,54 +64,6 @@ Page({
   getRandomColor: function () {
     var colors = ["pink", "orange", "green", "red", "blue", "mauve", "purple", "olive"];
     return colors[Math.floor(Math.random() * colors.length)];
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
+  }
 
 })
