c815f519f28d4b21b03c677414faa3d1b78436e4
saving <savingrun@gmail.com>
2023-06-13 14:44:56 +0800

    add getOpenId

---
+++ cloudfunctions/getOpenId/index.js
@@ -0,0 +1,16 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
