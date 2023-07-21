cdc9b4e72cc8d7e28eb0b53ef176b463bf7dc710
saving <savingrun@gmail.com>
2023-07-21 09:20:04 +0800

    opt `components`

--- miniprogram/components/authorCard/wh-authorCard.js
+++ miniprogram/components/authorCard/wh-authorCard.js
@@ -1,29 +1,15 @@
 // components/authorCard/wh-authorCard.js
 Component({
-    /**
-     * 组件的属性列表
-     */
     properties: {
         isHidden: {
             type: [Boolean, String],
             default: true
         }
     },
-
-    /**
-     * 组件的初始数据
-     */
     data: {
 
     },
-
-    /**
-     * 组件的方法列表
-     */
     methods: {
-        /**
-         * 防止冒泡
-         */
         prevent() {
             console.log("防止冒泡");
             var self = this;
@@ -33,7 +19,6 @@ Component({
 
         },
         showMask() {
-            // console.warn("showMask...")
             this.setData({
                 isHidden: false,
             });
@@ -48,7 +33,6 @@ Component({
             })
         },
         closeMask() {
-            // console.warn("closeMask...")
             var that = this;
             var animation = wx.createAnimation({
                 duration: 1000,
@@ -67,18 +51,12 @@ Component({
             }, 600);
         }
     },
-    /**
-     * 通过组件的生命周期函数执行代码
-     */
     lifetimes: {
         attached: function () {
-            // 在组件实例进入页面节点树时执行
             this.setData({
                 isHidden: true,
             });
         },
-        detached: function () {
-            // 在组件实例被从页面节点树移除时执行
-        },
+        detached: function () {},
     },
 })
--- miniprogram/components/typeWriter/wh-typeWriter.js
+++ miniprogram/components/typeWriter/wh-typeWriter.js
@@ -1,41 +1,20 @@
 // components/typeWriter/wh-typeWriter.js
 Component({
-    /**
-     * 组件的属性列表
-     */
     properties: {
         whContent: {
             type: String,
             default: ' '
         },
     },
-
-    /**
-     * 组件的初始数据
-     */
     data: {
     },
-
-    /**
-     * 组件的方法列表
-     */
     methods: {
     },
-
-    /**
-     * 通过组件的生命周期函数执行代码
-     */
     lifetimes: {
-        attached: function () {
-            // 在组件实例进入页面节点树时执行
-        },
-        detached: function () {
-            // 在组件实例被从页面节点树移除时执行
-        },
+        attached: function () {},
+        detached: function () {},
         ready: function () {
-            // 在组件在视图层布局完成后执行
             var that = this
-            //文字逐个显示
             var story = this.data.whContent;
             var i = 0;
             var time = setInterval(function () {
